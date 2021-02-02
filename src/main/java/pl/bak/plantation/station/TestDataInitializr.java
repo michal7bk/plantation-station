7866c5c0ca3775a5af95620f609bd1ddf43cefcd
mbak <michal.bak@cumulocity.com>
2021-02-02 11:54:59 +0100

    Additional test data

--- src/main/java/pl/bak/plantation/station/TestDataInitializr.java
+++ src/main/java/pl/bak/plantation/station/TestDataInitializr.java
@@ -6,7 +6,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import pl.bak.plantation.station.customer.domain.Customer;
 import pl.bak.plantation.station.customer.repository.CustomerRepository;
+import pl.bak.plantation.station.order.domain.Order;
+import pl.bak.plantation.station.order.repository.OrderRepository;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,23 +20,38 @@ public class TestDataInitializr implements ApplicationRunner {
 
     private final List<Customer> customers = new ArrayList<>();
     private final CustomerRepository customerRepository;
+    private final OrderRepository orderRepository;
     private Customer customer1;
     private Customer customer2;
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
     @Autowired
-    public TestDataInitializr(CustomerRepository customerRepository) {
+    public TestDataInitializr(CustomerRepository customerRepository, OrderRepository orderRepository) {
         this.customerRepository = customerRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) {
         cleanTables();
         createCustomers();
+        createOrders();
+    }
+
+    private void createOrders() {
+        Order order1 = Order.builder()
+                .completed(false)
+                .customer(customer1)
+                .creationTime(LocalDateTime.now())
+                .completedTime(LocalDateTime.now().plusMinutes(1L))
+                .additionalInfo("bla")
+                .build();
+        orderRepository.save(order1);
     }
 
     private void cleanTables() {
+        orderRepository.deleteAll();
         customerRepository.deleteAll();
     }
 
